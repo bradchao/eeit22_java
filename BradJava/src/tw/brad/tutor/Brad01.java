c61a20dc79f8345e5cf800931c5e12715e0c4d0a
mickeyyo55b <mickeyyo55b@gmail.com>
2026-04-29 12:37:17 +0800

    init

---
+++ BradJava/src/tw/brad/tutor/Brad01.java
@@ -0,0 +1,13 @@
+package tw.brad.tutor;
+
+public class Brad01 {
+	public static void main(String[] args) {
+		// byte 2^8 , short 2^16, int 2^32, long 2^64
+		byte var1;
+		byte var2 = 1;
+		int var3;
+		var1 = 126;
+		var3 = var1 + var2;
+		System.out.println(var3);
+	}
+}

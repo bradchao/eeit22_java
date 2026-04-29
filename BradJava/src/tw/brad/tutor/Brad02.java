b38ca96fa09fa3c67da2f65dee82a0c467c01015
mickeyyo55b <mickeyyo55b@gmail.com>
2026-04-29 14:16:25 +0800

    04291416

---
+++ BradJava/src/tw/brad/tutor/Brad02.java
@@ -0,0 +1,33 @@
+package tw.brad.tutor;
+
+import java.util.Scanner;
+
+public class Brad02 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("X = ");
+		int x = scanner.nextInt();
+		System.out.print("Y = ");
+		int y = scanner.nextInt();
+		System.out.println("-------");
+		
+		int r1 = x + y;
+		int r2 = x - y;
+		int r3 = x * y;
+		int r4 = x / y;
+		int r5 = x % y;
+		
+		System.out.printf("%d + %d = %d\n", x, y, r1);
+		System.out.printf("%d - %d = %d\n", x, y, r2);
+		System.out.printf("%d x %d = %d\n", x, y, r3);
+		System.out.printf("%d / %d = %d ...... %d\n", x, y, r4, r5);
+		
+		
+		
+		
+		
+	}
+
+}

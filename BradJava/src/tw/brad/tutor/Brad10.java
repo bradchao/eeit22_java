2dc776d977652e4b853e4bdf52d4012df69089f8
mickeyyo55b <mickeyyo55b@gmail.com>
2026-04-30 10:02:42 +0800

    04301002

--- BradJava/src/tw/brad/tutor/Brad10.java
+++ BradJava/src/tw/brad/tutor/Brad10.java
@@ -2,6 +2,13 @@ package tw.brad.tutor;
 
 public class Brad10 {
 	public static void main(String[] args) {
+		int a = 10, b = 3;
+		
+		if (++a <= 10 & --b >= 3) {
+			System.out.printf("OK: a = %d, b = %d", a, b);
+		}else {
+			System.out.printf("XX: a = %d, b = %d", a, b);
+		}
 		
 	}
 }

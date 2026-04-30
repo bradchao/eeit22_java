5fee71b1e1206ae1e74a6ce357280c4981e7c96f
mickeyyo55b <mickeyyo55b@gmail.com>
2026-04-30 12:00:05 +0800

    04301159

---
+++ BradJava/src/tw/brad/tutor/Brad12.java
@@ -0,0 +1,17 @@
+package tw.brad.tutor;
+
+public class Brad12 {
+
+	public static void main(String[] args) {
+		int a = 0;
+		while (a<10) {
+			System.out.println(a++);
+		}
+		//------------------
+		for (;a<10;) {
+			System.out.println(a++);
+		}
+
+	}
+
+}
---
+++ BradJava/src/tw/brad/tutor/Brad13.java
@@ -0,0 +1,22 @@
+package tw.brad.tutor;
+
+public class Brad13 {
+	public static void main(String[] args) {
+		int[] a;
+		a = new int[3];
+		a[0] = 123;
+		a[2] = 17;
+		a[1] = 333;
+		for (int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+		//System.out.println(a[100]);
+		System.out.println("---");
+		for (int vv : a) {	// for-each
+			System.out.println(vv);
+		}
+		
+		
+		
+	}
+}

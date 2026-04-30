8bdbc3aaa9132f2a8e26810cce76cf78561b4710
mickeyyo55b <mickeyyo55b@gmail.com>
2026-04-30 11:07:00 +0800

    04301106

--- BradJava/src/tw/brad/tutor/Brad11.java
+++ BradJava/src/tw/brad/tutor/Brad11.java
@@ -2,10 +2,15 @@ package tw.brad.tutor;
 
 public class Brad11 {
 	public static void main(String[] args) {
-		for (int k=0 ; k < 5; k++) {
+		final int ROWS = 1;
+		final int COLS = 9;
+		final int START = 1;
+		
+		//-------------------------
+		for (int k=0 ; k < ROWS; k++) {
 			for (int j = 1; j<=9 ; j++) {
-				for (int i = 2; i<= 5; i++) {
-					int newi = i + k * 4;
+				for (int i = START; i< START+COLS; i++) {
+					int newi = i + k * COLS;
 					int r = newi * j;
 					System.out.printf("%d x %d = %d\t", newi, j, r);
 				}

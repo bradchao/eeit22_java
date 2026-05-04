e7b399ceacca5684f1db6866791ffa340f574a94
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-04 11:08:08 +0800

    05041108

---
+++ BradJava/src/tw/brad/tutor/Brad18.java
@@ -0,0 +1,16 @@
+package tw.brad.tutor;
+
+public class Brad18 {
+	public static void main(String[] args) {
+		int a = 10;
+		while (a < 10) {
+			System.out.println(a++);
+		}
+		System.out.println("---");
+		a = 10;
+		do {
+			a++;
+			System.out.println(a);
+		}while (a<10);
+	}
+}

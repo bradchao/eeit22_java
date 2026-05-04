789589d1215cc71e2756dbfd7dd942430548cee4
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-04 16:32:46 +0800

    05041632

--- BradJava/src/tw/brad/apis/Scooter.java
+++ BradJava/src/tw/brad/apis/Scooter.java
@@ -2,6 +2,13 @@ package tw.brad.apis;
 
 public class Scooter extends Bike{
 	private int gear;
+	private String color;
+	
+	
+	public Scooter() {
+		System.out.println("OK");
+		color = "yellow";
+	}
 	
 	public int changeGear(int gear) {
 		if (gear >= 0 && gear <= 4) {
@@ -14,4 +21,9 @@ public class Scooter extends Bike{
 	public void upSpeed() {
 		speed = speed < 1? 1: speed * 1.8 * gear;
 	}
+	
+	public String getColor() {
+		return color;
+	}
+	
 }
--- BradJava/src/tw/brad/tutor/Brad21.java
+++ BradJava/src/tw/brad/tutor/Brad21.java
@@ -6,7 +6,7 @@ import tw.brad.apis.Scooter;
 public class Brad21 {
 
 	public static void main(String[] args) {
-		Bike b1 = new Bike();
+		//Bike b1 = new Bike();
 		Scooter s1 = new Scooter();
 		s1.changeGear(1);
 		s1.upSpeed();s1.upSpeed();s1.upSpeed();
@@ -15,7 +15,7 @@ public class Brad21 {
 		s1.downSpeed();
 		System.out.println(s1.getSpeed());
 		
-		
+		System.out.println(s1.getColor());
 		
 	}
 

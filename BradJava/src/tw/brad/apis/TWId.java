6db3b8d7cae2564d05ff01f6a4f0a63a68a3b256
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-05 13:57:45 +0800

    05051357

--- BradJava/src/tw/brad/apis/TWId.java
+++ BradJava/src/tw/brad/apis/TWId.java
@@ -24,8 +24,27 @@ public class TWId {
 		 * 192.168.3.4
 		 * A123456789
 		 */
+		
+		String letters = "ABCDEFGHJKLMNPQRSTUVXYWZIO";
 		if (id.matches("[A-Z][12][0-9]{8}")) {
-			ret = true;
+			char c1 = id.charAt(0);
+			int a12 = letters.indexOf(c1) + 10;
+			int a1 = a12 / 10;
+			int a2 = a12 % 10;
+			String s1 = id.substring(1, 2);
+			int n1 = Integer.parseInt(s1);
+			int n2 = Integer.parseInt(id.substring(2, 3));
+			int n3 = Integer.parseInt(id.substring(3, 4));
+			int n4 = Integer.parseInt(id.substring(4, 5));
+			int n5 = Integer.parseInt(id.substring(5, 6));
+			int n6 = Integer.parseInt(id.substring(6, 7));
+			int n7 = Integer.parseInt(id.substring(7, 8));
+			int n8 = Integer.parseInt(id.substring(8, 9));
+			int n9 = Integer.parseInt(id.substring(9, 10));
+			
+			int sum = a1*1 + a2*9 + n1*8 + n2*7 + n3*6 + n4*5 + n5*4 +
+					n6*3 + n7*2 + n8*1 + n9*1;
+			ret = sum % 10 == 0;
 		}
 		
 		return ret;

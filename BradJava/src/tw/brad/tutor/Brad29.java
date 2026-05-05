51fae458fd9267f217166d5bf3c72a347405f029
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-05 15:28:53 +0800

    05051528

---
+++ BradJava/src/tw/brad/tutor/Brad29.java
@@ -0,0 +1,22 @@
+package tw.brad.tutor;
+
+public class Brad29 {
+	public static void main(String[] args) {
+
+	}
+}
+
+class Brad291{
+	Brad291(){System.out.println("Brad291()");}
+}
+class Brad292 extends Brad291{
+	Brad292(){
+		this(1);
+	}
+	Brad292(int a){
+		this(1.2);
+	}
+	Brad292(double b){
+		
+	}
+}

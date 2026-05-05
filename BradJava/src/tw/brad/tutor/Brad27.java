32c7ff7d85871b218dcc3431b9045728b32db485
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-05 11:09:59 +0800

    05051109

---
+++ BradJava/src/tw/brad/tutor/Brad27.java
@@ -0,0 +1,12 @@
+package tw.brad.tutor;
+
+public class Brad27 {
+
+	public static void main(String[] args) {
+		String s1 = "Brad";
+		String s2 = s1.concat("Chao");
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+
+}

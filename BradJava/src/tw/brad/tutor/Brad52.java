cf27b6471af49486892346b544aedfc7fad1af8c
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-11 13:49:33 +0800

    05111349

---
+++ BradJava/src/tw/brad/tutor/Brad52.java
@@ -0,0 +1,21 @@
+package tw.brad.tutor;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Brad52 {
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		try(FileInputStream fin = new FileInputStream("dir1/coffee.jpg");
+			FileOutputStream fout = new FileOutputStream("dir2/coffee1.jpg")){
+			int len; byte[] buf = new byte[4*1024];
+			while ( (len = fin.read(buf)) != -1) {
+				fout.write(buf, 0, len);
+			}
+			System.out.println("Finish1");
+			System.out.println(System.currentTimeMillis() - start);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}

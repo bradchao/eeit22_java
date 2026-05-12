d01caf73be64b0c41677ff672ac966647a928e14
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-12 09:20:57 +0800

    05120920

--- BradJava/src/tw/brad/tutor/MySign.java
+++ BradJava/src/tw/brad/tutor/MySign.java
@@ -5,19 +5,25 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import tw.brad.apis.SignPanel;
 
 public class MySign extends JFrame{
 	private SignPanel panel;
-	private JButton clear, undo, redo, color, saveObj, loadObj;
+	private JButton clear, undo, redo, color, saveObj, saveAsObj, loadObj;
+	private File nowFile;
 	
 	public MySign() {
+		
+		nowFile = null;
 		
 		setLayout(new BorderLayout());
 		panel = new SignPanel();
@@ -29,9 +35,11 @@ public class MySign extends JFrame{
 		redo = new JButton("Redo");
 		color = new JButton("Color");
 		saveObj = new JButton("Save Obj");
+		saveAsObj = new JButton("SaveAs Obj");
 		loadObj = new JButton("Load Obj");
 		top.add(clear);top.add(undo);top.add(redo);
-		top.add(color);top.add(saveObj);top.add(loadObj);
+		top.add(color);
+		top.add(saveObj);top.add(saveAsObj);top.add(loadObj);
 		
 		add(top, BorderLayout.NORTH);
 		
@@ -67,12 +75,68 @@ public class MySign extends JFrame{
 				changeColor();
 			}
 		});
+		saveAsObj.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveAsObj();
+			}
+		});
+		saveObj.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveObj();
+			}
+		});
+		loadObj.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				loadObj();
+			}
+		});
 	}
 	
 	private void changeColor() {
 		Color newColor = JColorChooser.showDialog(this, "Change Color", panel.getColor());
 		if (newColor != null) {
 			panel.changeColor(newColor);
+		}
+	}
+	
+	private void saveObj() {
+		if (nowFile != null) {
+			try {
+				panel.saveLines(nowFile);
+				JOptionPane.showMessageDialog(this, "Save Success");
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(this, "Save Failure");
+			}
+		}else {
+			
+		}
+	}
+
+	private void saveAsObj() {
+		JFileChooser jfc = new JFileChooser();
+		if (jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+			nowFile = jfc.getSelectedFile();
+			try {
+				panel.saveLines(nowFile);
+				JOptionPane.showMessageDialog(this, "Save Success");
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(this, "Save Failure");
+			}
+		}
+	}
+	
+	private void loadObj() {
+		JFileChooser jfc = new JFileChooser();
+		if (jfc.showOpenDialog(this)== JFileChooser.APPROVE_OPTION) {
+			nowFile = jfc.getSelectedFile();
+			try {
+				panel.loadLine(nowFile);
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(this, "Load Failure");
+			}
 		}
 	}
 	

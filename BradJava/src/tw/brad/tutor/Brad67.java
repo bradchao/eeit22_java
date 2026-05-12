d2489abcf0ffcd3798f2693ca0e125ff2469d6ce
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-12 16:30:04 +0800

    05121629

---
+++ BradJava/src/tw/brad/tutor/Brad67.java
@@ -0,0 +1,46 @@
+package tw.brad.tutor;
+
+public class Brad67 {
+	public static void main(String[] args) {
+		Brad671 t1 = new Brad671("A");
+		Brad671 t2 = new Brad671("B");
+		Thread t3 = new Thread(new Brad672("C"));
+		t1.start();
+		t2.start();
+		t3.start();
+		System.out.println("main");
+	}
+}
+
+class Brad671 extends Thread{
+	String name;
+	Brad671(String name){this.name = name;}
+	@Override
+	public void run() {
+		for (int i= 0; i<20; i++) {
+			System.out.println(name + " " + i);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
+
+class Brad672 implements Runnable {
+	String name;
+	Brad672(String name){this.name = name;}
+
+	@Override
+	public void run() {
+		for (int i= 0; i<20; i++) {
+			System.out.println(name + " " + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+			}
+		}
+		
+	}
+	
+}

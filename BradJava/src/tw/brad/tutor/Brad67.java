3cde52eade284b35339e894b7bca09ac878f1fe1
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-13 11:59:39 +0800

    05131159

--- BradJava/src/tw/brad/tutor/Brad67.java
+++ BradJava/src/tw/brad/tutor/Brad67.java
@@ -5,7 +5,8 @@ public class Brad67 {
 		System.out.println("start");
 		Brad671 t1 = new Brad671("A");
 		Brad671 t2 = new Brad671("B");
-		Thread t3 = new Thread(new Brad672("C"));
+		Thread t3 = new Thread(new Brad672("C",t1));
+		
 		t1.start();
 		t2.start();
 		t3.start();
@@ -39,12 +40,19 @@ class Brad671 extends Thread{
 
 class Brad672 implements Runnable {
 	String name;
-	Brad672(String name){this.name = name;}
+	Brad671 t1;
+	Brad672(String name, Brad671 t1){this.name = name; this.t1 = t1;}
 
 	@Override
 	public void run() {
 		for (int i= 0; i<20; i++) {
 			System.out.println(name + " " + i);
+			if (i == 10) {
+				try {
+					t1.join();
+				} catch (InterruptedException e) {
+				}
+			}
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {

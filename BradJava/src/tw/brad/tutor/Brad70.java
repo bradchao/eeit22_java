4d5bf92a68fa3163a6c26ebd6d9182931b1bd3a7
mickeyyo55b <mickeyyo55b@gmail.com>
2026-05-13 13:45:42 +0800

    05131345

---
+++ BradJava/src/tw/brad/tutor/Brad70.java
@@ -0,0 +1,36 @@
+package tw.brad.tutor;
+
+public class Brad70 {
+
+	public static void main(String[] args) throws InterruptedException {
+		Counter counter = new Counter();
+		Thread t1 = new Thread(() -> {
+			for (int i=0; i<1000; i++) {
+				counter.add();
+			}
+		});
+		Thread t2 = new Thread(() -> {
+			for (int i=0; i<1000; i++) {
+				counter.add();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+		System.out.println(counter.count);
+		
+		
+		
+	}
+
+}
+class Counter {
+	int count;
+	public void add() {
+		count++;
+	}
+}

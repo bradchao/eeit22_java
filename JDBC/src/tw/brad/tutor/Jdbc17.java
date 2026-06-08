e7a691f9ceff0abc63994a5076aeb73cff47514f
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-08 09:37:27 +0800

    06080937

--- JDBC/src/tw/brad/tutor/Jdbc17.java
+++ JDBC/src/tw/brad/tutor/Jdbc17.java
@@ -1,11 +1,17 @@
 package tw.brad.tutor;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Properties;
 import java.util.Scanner;
 
+import tw.brad.apis.BCrypt;
+
 public class Jdbc17 {
 	private static final String URL = "jdbc:mysql://localhost:3306/brad";
-	private static final String USER = "brad";
+	private static final String USER = "root";
 	private static final String PASSWDS = "root";
 	private static final String SQL_REG = """
 			INSERT INTO member
@@ -34,25 +40,53 @@ public class Jdbc17 {
 		System.out.print("Password: ");
 		String passwd = scanner.nextLine();
 		
-		prop.put("user", USER);prop.put("password", PASSWDS);
+		prop.put("user", USER);
+		prop.put("password", PASSWDS);
+		prop.put("useSSL", false);
 		
-		if (!isAccountExist(account)) {
-			if (regMember(account, passwd)) {
-				System.out.println("Register Success");
+		try {
+			if (!isAccountExist(account)) {
+				if (regMember(account, passwd)) {
+					System.out.println("Register Success");
+				}else {
+					System.out.println("Register Failure");
+				}
 			}else {
-				System.out.println("Register Failure");
+				System.out.println("Account EXIST!");
 			}
-		}else {
-			System.out.println("Account EXIST!");
+		}catch(Exception e) {
+			System.out.println(e);
 		}
 	}
 	
-	private static boolean isAccountExist(String account) {
-		return true;
+	private static boolean isAccountExist(String account) throws Exception{
+		/*
+		try(Connection conn = DriverManager.getConnection(URL, prop);
+			PreparedStatement pstmt = conn.prepareStatement(SQL_CHECK1)){
+			pstmt.setString(1, account);
+			ResultSet rs = pstmt.executeQuery();
+			return rs.next();
+		}
+		*/
+		
+		try(Connection conn = DriverManager.getConnection(URL, prop);
+			PreparedStatement pstmt = conn.prepareStatement(SQL_CHECK2)){
+			pstmt.setString(1, account);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			return rs.getInt("count") > 0;
+		}
+		
 	}
 	
-	private static boolean regMember(String account, String passwd) {
-		return true;
+	private static boolean regMember(String account, String passwd) throws Exception {
+		try(Connection conn = DriverManager.getConnection(URL, prop);
+			PreparedStatement pstmt = conn.prepareStatement(SQL_REG)){
+			pstmt.setString(1, account);
+			pstmt.setString(2, BCrypt.hashpw(passwd, BCrypt.gensalt()));
+			int n = pstmt.executeUpdate();
+			return n > 0;
+		}
 	}
 
 }

1e50c26ac183f85828468da237b843fae39cbd16
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-10 10:00:38 +0800

    06101000

---
+++ BradWeb2/src/main/java/tw/brad/servlet/Brad14.java
@@ -0,0 +1,29 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Brad14")
+public class Brad14 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("Brad15");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Brad Big Company: Brad14</h1>");
+		out.println("<hr />");
+		
+		dispatcher.forward(request, response);
+		
+		out.println("<div>Footer</div>");	
+	}
+
+}
---
+++ BradWeb2/src/main/java/tw/brad/servlet/Brad15.java
@@ -0,0 +1,22 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Brad15")
+public class Brad15 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Brad Big Company: 15</h1>");
+		out.println("<hr />");
+		out.println("<div>Footer</div>");	
+	
+	}
+
+}

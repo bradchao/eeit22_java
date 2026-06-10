416c549b5f14c8fc272ae8e0cf5a421b58a19695
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-10 09:40:29 +0800

    06100940

---
+++ BradWeb2/src/main/java/tw/brad/servlet/Brad12.java
@@ -0,0 +1,35 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Brad12")
+public class Brad12 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("Brad13");
+		
+		request.setAttribute("y", 35);
+		
+		
+		//----------------
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Brad Big Company</h1>");
+		
+		out.println("<hr />");
+		dispatcher.include(request, response);
+		out.println("<hr />");
+		
+		out.println("<div>Footer</div>");
+
+	}
+
+}
---
+++ BradWeb2/src/main/java/tw/brad/servlet/Brad13.java
@@ -0,0 +1,26 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Brad13")
+public class Brad13 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String x = request.getParameter("x");
+		//String y = request.getParameter("y");
+		Integer y = (Integer)request.getAttribute("y");
+		
+		PrintWriter out = response.getWriter();
+		out.println("Brad13");
+		out.printf("x = %s<br />", x);
+		out.printf("y = %d<br />", y);
+	
+	}
+
+}

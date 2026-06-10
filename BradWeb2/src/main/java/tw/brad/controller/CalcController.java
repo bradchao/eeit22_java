c265aef842219f528a72e9480ab884ed73e0e104
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-10 10:06:20 +0800

    06101006

---
+++ BradWeb2/src/main/java/tw/brad/controller/CalcController.java
@@ -0,0 +1,20 @@
+package tw.brad.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/CalcController")
+public class CalcController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. Client
+		
+		// 2. Model
+		
+		// 3. Viewer => forward
+	}
+
+}

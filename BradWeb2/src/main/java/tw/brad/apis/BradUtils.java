bb2baea23052401e031aa18bb0c4045bc3833b6e
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-15 13:42:44 +0800

    06151342

--- BradWeb2/src/main/java/tw/brad/apis/BradUtils.java
+++ BradWeb2/src/main/java/tw/brad/apis/BradUtils.java
@@ -6,6 +6,11 @@ import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Random;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class BradUtils {
 	public static String calc(String x, String y) {
@@ -21,6 +26,27 @@ public class BradUtils {
 	public static int createScore() {
 		return new Random().nextInt(101);
 	}
+	
+	public static SortedMap[] parseGift(String json) {
+		JSONArray root = new JSONArray(json);
+		TreeMap<String, String>[] gifts = new TreeMap[root.length()];
+		
+		for (int i=0; i<root.length(); i++) {
+			JSONObject gift = root.getJSONObject(i);
+			TreeMap<String, String> map = new TreeMap<>();
+			map.put("name", gift.getString("Name"));
+			map.put("city", gift.getString("County"));
+			map.put("town", gift.getString("Township"));
+			map.put("addr", gift.getString("SalePlace"));
+			map.put("tel", gift.getString("ContactTel"));
+			map.put("picurl", gift.getString("Column1"));
+			gifts[i] = map;
+		}
+		
+		
+		return gifts;
+	}
+	
 	public static String loadView() throws Exception {
 		String source = "C:\\Users\\User\\git\\repository\\BradWeb2\\src\\main\\webapp\\views\\view1.html";
 		File html = new File(source);

397d382cc790ce38120315c97e05abdc8fc02af2
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-16 15:11:12 +0800

    06161511

---
+++ hi1/src/main/java/tw/brad/utils/HibernateUtil.java
@@ -0,0 +1,19 @@
+package tw.brad.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			
+			sessionFactory = cfg.buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+	
+}

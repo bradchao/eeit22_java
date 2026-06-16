c01e1103a4d3cb48890c5b34d42286d9dc4fabe6
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-16 15:39:18 +0800

    06161539

--- hi1/src/main/java/tw/brad/utils/HibernateUtil.java
+++ hi1/src/main/java/tw/brad/utils/HibernateUtil.java
@@ -3,6 +3,8 @@ package tw.brad.utils;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import tw.brad.entity.Test;
+
 public class HibernateUtil {
 	private static SessionFactory sessionFactory;
 
@@ -10,6 +12,8 @@ public class HibernateUtil {
 		if (sessionFactory == null) {
 			Configuration cfg = new Configuration();
 			cfg.configure("hibernate.cfg.xml");
+			
+			cfg.addAnnotatedClass(Test.class);
 			
 			sessionFactory = cfg.buildSessionFactory();
 		}

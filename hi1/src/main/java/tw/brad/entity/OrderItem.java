aa00c841ccc76742a18719fb7764f6c528633fad
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-17 12:00:00 +0800

    06171159

---
+++ hi1/src/main/java/tw/brad/entity/OrderItem.java
@@ -0,0 +1,78 @@
+package tw.brad.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "oitem")
+public class OrderItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String pname;
+	
+	@Column(nullable = false)
+	private int price;
+	
+	@Column(nullable = false)
+	private int qty;
+
+	public OrderItem() {}
+	public OrderItem(String pname, int price, int qty) {
+		this.pname = pname; this.price = price; this.qty = qty;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	//------------------------
+	private Order order;
+
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
+
+	
+}

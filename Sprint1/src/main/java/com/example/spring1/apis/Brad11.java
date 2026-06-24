1e04716e600c7be254b2e3c0ab1d6c616f7ef824
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-24 10:37:22 +0800

    06241037

--- Sprint1/src/main/java/com/example/spring1/apis/Brad11.java
+++ Sprint1/src/main/java/com/example/spring1/apis/Brad11.java
@@ -1,5 +1,7 @@
 package com.example.spring1.apis;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,8 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring1.dto.Order;
+import com.example.spring1.dto.OrderDetail;
 
 @RestController
 @RequestMapping("/north")
@@ -30,7 +36,39 @@ public class Brad11 {
 	}
 	
 	@GetMapping(value = {"/orders", "/orders/{orderId}"})
-	public List<Order> test2() {
+	public List<Order> test2(@PathVariable(required = false) Integer orderId) {
+		String sql = """
+				SELECT o.OrderID id, o.OrderDate odate,
+					p.ProductName pname, 
+					od.UnitPrice price, od.Quantity qty
+				FROM orders o
+				JOIN orderdetails od ON o.OrderID = od.OrderID
+				JOIN products p ON od.ProductID = p.ProductID
+				WHERE o.OrderID = :orderId
+				""";
+		HashMap<String, Integer> params = new HashMap<>();
+		params.put("orderId", orderId);
+		
+		List<Order> orders = new ArrayList<>();
+		
+		List<Map<String,Object>> details = jdbc.queryForList(sql, params);
+		
+		Order order = new Order();
+		order.setOrderId((Integer)details.get(0).get("id"));
+		order.setOrderDate(((LocalDateTime)(details.get(0).get("odate"))).toString());
+		
+		for (Map<String,Object> detail: details) {
+			OrderDetail od = new OrderDetail();
+			od.setId((Integer)detail.get("id"));
+			od.setPname((String)detail.get("pname"));
+			od.setPrice(Double.parseDouble(detail.get("price").toString()));
+			od.setQty(Integer.parseInt(detail.get("qty").toString()));
+			order.getDetails().add(od);
+		}
+		
+		orders.add(order);
+		
+		return orders;
 		
 	}
 	

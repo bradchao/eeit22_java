9a431e93997500702f9cb6b8682e413d3d96544a
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-26 11:59:35 +0800

    06261159

--- Sprint3/src/main/java/com/example/spring3/controller/HotelController.java
+++ Sprint3/src/main/java/com/example/spring3/controller/HotelController.java
@@ -1,11 +1,18 @@
 package com.example.spring3.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.spring3.entity.Hotel;
 import com.example.spring3.repository.HotelRepository;
 
 @RestController
@@ -15,14 +22,22 @@ public class HotelController {
 	private HotelRepository repository;
 
 	@GetMapping("")
-	public void queryHotelByPage(
+	public ResponseEntity<Map<String,Object>> queryHotelByPage(
 			@RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "10") int rpp) {
 		
+		Pageable pageable = PageRequest.of(page, rpp);
+		Page<Hotel> hotelPage = repository.findAll(pageable);
 		
+		Map<String,Object> result = Map.of(
+			"data", hotelPage.getContent(),
+			"total", hotelPage.getTotalElements(),
+			"totalPage", hotelPage.getTotalPages(),
+			"page", hotelPage.getNumber(),
+			"isLast", hotelPage.isLast()
+				);
 		
-		
-		
+		return ResponseEntity.ok(result);
 	}
 	
 	

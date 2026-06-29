58a9a6188895c0cfcba80e8537e2fa5fd15070c8
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-29 13:56:26 +0800

    06291356

--- Sprint4/src/main/java/com/example/spring4/repo/EmployeeRepo.java
+++ Sprint4/src/main/java/com/example/spring4/repo/EmployeeRepo.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.spring4.entity.Employee;
+import com.example.spring4.projection.EmployeeProjection;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
-	List<Employee> searchByTitleStartingWith(String start);
+	List<EmployeeProjection> searchByTitleStartingWith(String start);
 }

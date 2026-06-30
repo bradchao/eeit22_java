d882bdd4046b6b1f9f194c9009bbf3678a306f04
mickeyyo55b <mickeyyo55b@gmail.com>
2026-06-30 11:57:51 +0800

    06301157

--- Spring5/src/main/java/com/example/spring5/controller/WebController.java
+++ Spring5/src/main/java/com/example/spring5/controller/WebController.java
@@ -1,8 +1,14 @@
 package com.example.spring5.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.spring5.apis.User;
 
 /*
  * request => Controller
@@ -36,7 +42,26 @@ public class WebController {
 	@RequestMapping("/page1")
 	public String page1(Model model) {
 		model.addAttribute("companyName", "Brad Big Company");
+		model.addAttribute("userName", "Brad");
+		
+		User user = new User();
+		user.setName("Kevin");
+		user.setGender(true);
+		user.setAge(18);
+		
+		model.addAttribute("user", user);
+		
+		String now = LocalDateTime.now().format(
+				DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+		model.addAttribute("now", now);
+		
 		return "page1";
+	}
+	
+	@RequestMapping("/page2/{status}")
+	public String page2(Model model, @PathVariable String status) {
+		model.addAttribute("status", status);
+		return "page2";
 	}
 	
 	

2edcace8237d3a1b90e50bcbd29e33586ebb9a9f
mickeyyo55b <mickeyyo55b@gmail.com>
2026-07-07 10:37:11 +0800

    07071037

--- Spring10/src/main/java/com/example/spring10/Spring10Application.java
+++ Spring10/src/main/java/com/example/spring10/Spring10Application.java
@@ -5,14 +5,10 @@ import javax.swing.JFrame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.spring10.enums.OrderStatus;
+
 @SpringBootApplication
-public class Spring10Application extends JFrame{
-	public Spring10Application() {
-		
-		setSize(640, 480);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+public class Spring10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring10Application.class, args);
